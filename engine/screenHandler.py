607a6130fb9a822c47b2319d3702507e9e2d00b1
davidw <61147505+davidwingqvist@users.noreply.github.com>
2024-11-18 11:36:47 +0900

    Update screenHandler.py
    squares to

--- engine/screenHandler.py
+++ engine/screenHandler.py
@@ -9,15 +9,17 @@ class ScreenHandler:
         xBlocks = math.floor(globals.SCREEN_WIDTH / globals.BLOCKS_PER_WIDTH)
         yBlocks = math.floor(globals.SCREEN_HEIGHT / globals.BLOCKS_PER_HEIGHT)
 
+        self.squares_to_y = globals.SCREEN_HEIGHT // yBlocks
+        self.squares_to_x = globals.SCREEN_WIDTH // xBlocks
 
 
         x = 0
         y = 0
-        for i in range(globals.SCREEN_HEIGHT // yBlocks):
+        for i in range(self.squares_to_y):
             x = 0
             y += yBlocks
 
-            for i in range(globals.SCREEN_WIDTH // xBlocks):
+            for i in range(self.squares_to_x):
                 x += xBlocks
                 square = ScreenSquare(x, y)
                 self.squares.append(square)
